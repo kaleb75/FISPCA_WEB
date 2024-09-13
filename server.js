6e07a2abb1e6072a7c2884d887233546a142e71b
kaleb75 <44348427+kaleb75@users.noreply.github.com>
2024-09-13 08:27:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,11 +39,48 @@ app.get('/api/consulta', async (req, res) => {
                        NWC,
                        (SELECT Description FROM STATION WHERE STATION.WC = PCA_SNO.NWC) AS Next_Station
                 FROM PCA_SNO
+                WHERE McbSno = @serialNumber;
+
+                -- Union of multiple queries to retrieve information from various tables based on McbSno
+                SELECT InputType, IsPass, BadgeNo AS Editor, McbSno AS SN, PdLine AS Line, Cdt AS Creation_Date, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = SMT_LOG.WC) AS STATION
+                FROM SMT_LOG
                 WHERE McbSno = @serialNumber
+                UNION
+                SELECT Input_Type, IsPass, BadgeNo, McbSno, PdLine, Cdt, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = PCA_LOG.WC) AS STATION
+                FROM PCA_LOG 
+                WHERE McbSno = @serialNumber
+                UNION
+                SELECT Input_Type, IsPass, BadgeNo, McbSno, PdLine, Cdt, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = API_LOG.WC) AS STATION
+                FROM API_LOG
+                WHERE McbSno = @serialNumber
+                UNION
+                SELECT SPType, IsPass, BadgeNo, SNO, PdLine, Cdt, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = PCA_WC_LOG.WC) AS STATION
+                FROM PCA_WC_LOG
+                WHERE SNO = @serialNumber
+                UNION
+                SELECT Input_Type, IsPass, BadgeNo, McbSno, PdLine, Cdt, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = SA_LOG.WC) AS STATION 
+                FROM SA_LOG
+                WHERE McbSno = @serialNumber
+                UNION
+                SELECT Status, LogId, BadgeNo, McbSno, PdLine, Cdt, WC, 
+                       (SELECT Description FROM STATION WHERE STATION.WC = AUTOSCAN_PIC_LOG.WC) AS STATION 
+                FROM AUTOSCAN_PIC_LOG
+                WHERE McbSno = @serialNumber
+                UNION
+                SELECT ShipTo, Status, Editor, Sno, Rev, Cdt, CartonNo, 
+                       (SELECT Dn FROM SHIPCTRL_CARTON_DN_COMBINE WHERE SHIPCTRL_PACKING.CartonNo = SHIPCTRL_CARTON_DN_COMBINE.CartonNo) AS STATION 
+                FROM SHIPCTRL_PACKING
+                WHERE Sno = @serialNumber
+                ORDER BY Cdt ASC;
             `);
 
         // Enviar los resultados como respuesta JSON
-        res.json(result.recordset);
+        res.json(result.recordsets);
     } catch (error) {
         console.error('Error al ejecutar la consulta:', error);
         res.status(500).send('Error al ejecutar la consulta');
