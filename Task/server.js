6292b8583460b00d7ebc29ee3c5a2b3fd025d250
kaleb75 <44348427+kaleb75@users.noreply.github.com>
2024-09-25 17:23:09 -0600

    Update server.js

--- Task/server.js
+++ Task/server.js
@@ -1,71 +1,24 @@
-// Importar módulos necesarios
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 4000;
-const { getItems, getItemById, createItem, updateItem, deleteItem } = require('./queries');
+const port = 3000;
+const db = require('./queries');
 
-// Middleware para procesar datos JSON y solicitudes URL-encoded
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // Servir archivos estáticos del directorio 'public'
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
-// Ruta para obtener todos los ítems
-app.get('/items', async (req, res) => {
-    try {
-        const items = await getItems();
-        res.status(200).json(items);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener ítems' });
-    }
-});
+app.use(express.static('public'));
 
-// Ruta para obtener un ítem por ID
-app.get('/items/:id', async (req, res) => {
-    try {
-        const item = await getItemById(req.params.id);
-        if (item) {
-            res.status(200).json(item);
-        } else {
-            res.status(404).json({ message: 'Ítem no encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener ítem' });
-    }
-});
+app.get('/tasks', db.getTasks);
+app.get('/tasks/:id', db.getTaskById);
+app.post('/tasks', db.createTask);
+app.put('/tasks/:id', db.updateTask);
+app.delete('/tasks/:id', db.deleteTask);
 
-// Ruta para crear un nuevo ítem
-app.post('/items', async (req, res) => {
-    try {
-        const newItem = req.body;
-        await createItem(newItem);
-        res.status(201).json({ message: 'Ítem creado exitosamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al crear ítem' });
-    }
-});
-
-// Ruta para actualizar un ítem
-app.put('/items/:id', async (req, res) => {
-    try {
-        const updatedItem = req.body;
-        await updateItem(req.params.id, updatedItem);
-        res.status(200).json({ message: 'Ítem actualizado exitosamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al actualizar ítem' });
-    }
-});
-
-// Ruta para eliminar un ítem
-app.delete('/items/:id', async (req, res) => {
-    try {
-        await deleteItem(req.params.id);
-        res.status(200).json({ message: 'Ítem eliminado exitosamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar ítem' });
-    }
-});
-
-// Iniciar el servidor
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`App running on port ${port}.`);
 });
