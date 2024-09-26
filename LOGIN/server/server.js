7bf49a202a272fc9d1024c7ff209ca238a913fe8
kaleb75 <44348427+kaleb75@users.noreply.github.com>
2024-09-26 09:03:37 -0600

    Create server.js

---
+++ LOGIN/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
