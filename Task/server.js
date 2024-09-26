4310a58f336c5725439aa928dbe93a99b3bd95df
kaleb75 <44348427+kaleb75@users.noreply.github.com>
2024-09-26 14:11:44 -0600

    Update server.js

--- Task/server.js
+++ Task/server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const db = require('./queries');
+const cron = require('node-cron');
+const { archiveCompletedTasks } = require('./queries');
+
 
 // Middleware para parsear el cuerpo de las solicitudes
 app.use(bodyParser.json());
@@ -26,4 +29,15 @@ app.delete('/tasks/:id', db.deleteTask);
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
     console.log(`La aplicacion esta corriendo ejecuta en tu navegador http://localhost:${port}.`);
+});
+
+app.get('/tasks/completed', async (request, response) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query('SELECT * FROM tasks_archive ORDER BY completed_at DESC');
+        response.status(200).json(result.recordset);
+    } catch (error) {
+        console.error('Error al obtener tasks completadas:', error);
+        response.status(500).send('Error al obtener tasks completadas');
+    }
 });
